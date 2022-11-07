0d601d3fc91390dccc957d4bfbb93e7487c36404
hexcowboy <accounts@cowboy.dev>
2022-11-07 07:29:09 -0700

    ignore precompile addresses and console address

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.13;
 
 import "@forge-std/Test.sol";
+import "@forge-std/StdCheats.sol";
 import "../src/Counter.sol";
 
 contract CounterTest is Test {
@@ -14,6 +15,8 @@ contract CounterTest is Test {
     function testFuzzPayable(address[10] memory senders, uint256[10] memory amounts) public {
         for (uint256 i = 0; i < amounts.length; i++) {
             amounts[i] = bound(amounts[i], 0, 500);
+            vm.assume(senders[i] != 0x000000000000000000636F6e736F6c652e6c6f67);
+            StdCheatsSafe.assumeNoPrecompiles(senders[i]);
         }
 
         // add funds to contract
